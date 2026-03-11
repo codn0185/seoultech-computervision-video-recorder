e4731b595746b11564f61f7cda42cdfbbd1e11bd
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-03-11 17:32:05 +0900

    feat: init main.py
    create main.py file and import libraries

---
+++ main.py
@@ -0,0 +1,11 @@
+# 서울과학기술대학교 컴퓨터비전 2주차 과제
+# Video Recorder
+
+# Import Libraries
+import numpy as np
+import cv2 as cv
+
+print("---Version---")
+print("numpy: " + np.__version__)  # 2.4.3
+print("cv2: " + cv.__version__)  # 4.13.0
+print("-------------")
