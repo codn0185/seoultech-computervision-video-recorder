50eb04601a743ccfc0f776efe13fad55a871cb28
codn0185 <113341200+codn0185@users.noreply.github.com>
2026-03-11 17:51:43 +0900

    feat: Add configs & main() function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # 서울과학기술대학교 컴퓨터비전 2주차 과제
 # Video Recorder
 
-# Import Libraries
+# === Import Libraries ===
 import numpy as np
 import cv2 as cv
 
@@ -9,3 +9,27 @@ print("---Version---")
 print("numpy: " + np.__version__)  # 2.4.3
 print("cv2: " + cv.__version__)  # 4.13.0
 print("-------------")
+
+
+# === Config ===
+# Video Config
+VIDEO_URL = "rtsp://210.99.70.120:1935/live/cctv001.stream"  # CCTV001	127.1504447	36.8625719	세집매 삼거리	충청남도 천안시 서북구 신당동 482-22
+WIDTH: int
+HEIGHT: int
+FPS: int
+# Mod Config
+PREVIEW_MOD: int = 1
+RECORDER_MOD: int = 2
+# key Config
+SWITCH_MOD_KEY: int = 32  # Space
+EXIT_PROGRAM_KEY: int = 27  # ESC
+
+
+# === Main Function ===
+def main():
+    pass
+
+
+# === Run ===
+if __name__ == "__main__":
+    main()
